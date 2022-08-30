15d181aeff0c2142aa694d9fe2fd703c07567261
akunsk <akunskingsley@gmail.com>
2022-08-30 10:50:59 -0700

    5-variable_load.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
 import hidden_4
-
-
-def discovr():
-    name = dir(hidden_4)
-    for i in name:
-        if i[:2] != '__':
-            print("{:s}".format(i))
-
+funcs = dir(hidden_4)
 
 if __name__ == "__main__":
-    discovr()
+    for f in funcs:
+        if '__' not in f:
+            print(f)
