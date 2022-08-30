0c1046292bc4ef932d116e8273fa0edc0e04b142
akunsk <akunskingsley@gmail.com>
2022-08-30 11:00:01 -0700

    5-variable_load.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-def add_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d}".format(n))
-        return
-    else:
-        i = 1
-        add = 0
-        while i <= n:
-            add += int(argv[i])
-            i += 1
-        print("{:d}".format(add))
+from sys import argv
+
+
+def main():
+    i = 0
+    for j in range(1, len(argv)):
+        i += int(argv[j])
+    print('{}'.format(i))
+
 
 if __name__ == "__main__":
-    import sys
-    add_arg(sys.argv)
+    main()
